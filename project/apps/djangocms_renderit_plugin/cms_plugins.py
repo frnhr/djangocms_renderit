5661f4049e38b527176294ef23898ec51db1d33e
Fran <fran@changeset.hr>
2015-05-29 16:45:01 +0200

    cleanup

--- project/apps/djangocms_renderit_plugin/cms_plugins.py
+++ project/apps/djangocms_renderit_plugin/cms_plugins.py
@@ -1,7 +1,6 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from cms.models.pluginmodel import CMSPlugin
-# from django.utils.translation import ugettext_lazy as _
 
 
 class RenderItPlugin(CMSPluginBase):
@@ -9,12 +8,6 @@ class RenderItPlugin(CMSPluginBase):
     render_template = "renderit_plugin/renderit_plugin.html"
     cache = False
     allow_children = True
-
-
-    def render(self, context, instance, placeholder):
-        context = super().render(context, instance, placeholder)
-        context['content'] = "qweQWEQWEQ"
-        return context
 
 
 plugin_pool.register_plugin(RenderItPlugin)
