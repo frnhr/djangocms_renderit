32be862f80df24914e69a67634787d2bc7f64896
Fran <fran@changeset.hr>
2015-05-30 22:07:45 +0200

    test custom tag with settings

--- project/apps/djangocms_renderit_plugin/tests.py
+++ project/apps/djangocms_renderit_plugin/tests.py
@@ -1,10 +1,8 @@
+from django.test import TestCase, override_settings
 from cms.plugin_rendering import PluginContext
-from django.test import TestCase
-
 from cms.api import add_plugin
 from cms.models import Placeholder
 from djangocms_text_ckeditor.cms_plugins import TextPlugin
-
 from .cms_plugins import RenderItPlugin
 
 
@@ -73,3 +71,25 @@ class RenderitPluginTests(TestCase):
             placeholder=placeholder,
         ))
         self.assertEqual('admin: /en/admin/', html)
+
+    @override_settings(CMS_RENDERIT_TAG_LIBRARIES=('sample_tags', ))
+    def test_text_plugin_with_custom_tag(self):
+        placeholder = Placeholder.objects.create()
+        renderit_instance = add_plugin(
+            placeholder,
+            RenderItPlugin,
+            'en',
+        )
+        text_instance = add_plugin(
+            placeholder,
+            TextPlugin,
+            'en',
+            body='sample: {% sample %}',
+        )
+        renderit_instance.child_plugin_instances = [text_instance, ]
+        html = renderit_instance.render_plugin(PluginContext(
+            dict={'request': None},
+            instance=renderit_instance,
+            placeholder=placeholder,
+        ))
+        self.assertIn('HERE BE DA SIMPLE TAG!', html)
