492a5124024c1239f46040c5a7304d3ff6d54c0e
Fran <fran@changeset.hr>
2015-05-30 20:33:47 +0200

    simple text plugin tests

--- project/apps/djangocms_renderit_plugin/tests.py
+++ project/apps/djangocms_renderit_plugin/tests.py
@@ -1,7 +1,9 @@
+from cms.plugin_rendering import PluginContext
 from django.test import TestCase
 
 from cms.api import add_plugin
 from cms.models import Placeholder
+from djangocms_text_ckeditor.cms_plugins import TextPlugin
 
 from .cms_plugins import RenderItPlugin
 
@@ -28,4 +30,46 @@ class RenderitPluginTests(TestCase):
             'en',
         )
         html = model_instance.render_plugin({})
-        self.assertEqual(html, '')
+        self.assertEqual('', html)
+
+    def test_simple_text_plugin(self):
+        placeholder = Placeholder.objects.create()
+        renderit_instance = add_plugin(
+            placeholder,
+            RenderItPlugin,
+            'en',
+        )
+        text_instance = add_plugin(
+            placeholder,
+            TextPlugin,
+            'en',
+            body='just some text',
+        )
+        renderit_instance.child_plugin_instances = [text_instance, ]
+        html = renderit_instance.render_plugin(PluginContext(
+            dict={'request': None},
+            instance=renderit_instance,
+            placeholder=placeholder,
+        ))
+        self.assertEqual('just some text', html)
+
+    def test_text_plugin_with_url(self):
+        placeholder = Placeholder.objects.create()
+        renderit_instance = add_plugin(
+            placeholder,
+            RenderItPlugin,
+            'en',
+        )
+        text_instance = add_plugin(
+            placeholder,
+            TextPlugin,
+            'en',
+            body='admin: {% url "admin:index" %}',
+        )
+        renderit_instance.child_plugin_instances = [text_instance, ]
+        html = renderit_instance.render_plugin(PluginContext(
+            dict={'request': None},
+            instance=renderit_instance,
+            placeholder=placeholder,
+        ))
+        self.assertEqual('admin: /en/admin/', html)
