b2c5e2442649eedad64342dfad569930c4ce1ed9
Fran <fran@changeset.hr>
2015-05-30 22:31:15 +0200

    model field

--- project/apps/djangocms_renderit_plugin/models.py
+++ project/apps/djangocms_renderit_plugin/models.py
@@ -3,6 +3,8 @@ from cms.models import CMSPlugin
 
 
 class RenderitCMSPlugin(CMSPlugin):
+    tag_libraries = models.CharField(max_length=255, null=False, blank=True, default='',
+                                     help_text='Custom tag libraries, space-separated')
 
     def render_plugin(self, context=None, placeholder=None, admin=False, processors=None):
         try:
