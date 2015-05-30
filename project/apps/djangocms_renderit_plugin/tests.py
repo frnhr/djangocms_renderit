15c2760bd4c5cc6b5968dca9a572612909a27acc
Fran <fran@changeset.hr>
2015-05-30 22:08:50 +0200

    some cleanup

--- project/apps/djangocms_renderit_plugin/tests.py
+++ project/apps/djangocms_renderit_plugin/tests.py
@@ -8,18 +8,6 @@ from .cms_plugins import RenderItPlugin
 
 class RenderitPluginTests(TestCase):
 
-    # def test_plugin_context(self):
-    #     placeholder = Placeholder.objects.create(slot='test')
-    #     model_instance = add_plugin(
-    #         placeholder,
-    #         RenderItPlugin,
-    #         'en',
-    #     )
-    #     plugin_instance = model_instance.get_plugin_class_instance()
-    #     context = plugin_instance.render({}, model_instance, None)
-    #     self.assertIn('key', context)
-    #     self.assertEqual(context['key'], 'value')
-
     def test_empty_plugin(self):
         placeholder = Placeholder.objects.create()
         model_instance = add_plugin(
